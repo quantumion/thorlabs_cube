678681ba6f6a82afd04c044aa36177ed2ced04f8
Collin Epstein <cepstein@uwaterloo.ca>
2024-07-16 17:12:09 -0400

    improve installation and repository structure
    Issue #6
    -add version numbers to all dependencies
    -add metadata including version number to installation script
    -move source code into src directory

--- setup.py
+++ setup.py
@@ -2,19 +2,25 @@ from setuptools import setup, find_packages
 
 setup(
     name="thorlabs_cube",
+    version="0.0.1",
+    url="https://github.com/quantumion/thorlabs_cube",
+    description="ARTIQ controller for Thorlabs T/KCube devices",
     install_requires=[
-        "sipyco@git+https://github.com/m-labs/sipyco.git",
-        "asyncserial",
-        "numpy", # hidden sipyco dependency
+        "sipyco@git+https://github.com/m-labs/sipyco.git@v1.7",
+        "asyncserial==0.1.0",
+        "numpy==2.0.0", # hidden sipyco dependency
     ],
     extras_require={
         "docs": [
-            "sphinx",
-            "sphinx-argparse",
-            "sphinx-autodoc-typehints",
+            "sphinx==7.3.7",
+            "sphinx-argparse==0.4.0",
+            "sphinx-autodoc-typehints==2.2.2",
         ]
     },
-    packages=find_packages(),
+    packages=find_packages(
+        where="src",
+    ),
+    package_dir={"": "src"},
     entry_points={
         "console_scripts": [
             "aqctl_thorlabs_cube = thorlabs_cube.aqctl_thorlabs_cube:main",
