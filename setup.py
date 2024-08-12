951172689d5b9cc1b8d4cd77e05a4fc93a4217be
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2024-08-12 01:58:12 -0400

    testing and dependency changes
    Issue #4
    - remove unnecessary triggers
    - fix truthy warning for yamllint
    - organize dependencies in setup.py file for
      linting and type checking
    - use unittest for testing instead of pytest

--- setup.py
+++ setup.py
@@ -8,13 +8,20 @@ setup(
     install_requires=[
         "sipyco@git+https://github.com/m-labs/sipyco.git@v1.7",
         "asyncserial==0.1.0",
-        "numpy==2.0.0", # hidden sipyco dependency
+        "numpy==2.0.0",  # hidden sipyco dependency
     ],
     extras_require={
         "docs": [
             "sphinx==7.3.7",
             "sphinx-argparse==0.4.0",
             "sphinx-autodoc-typehints==2.2.2",
+        ],
+        "lint": [
+            "flake8==7.0.0",
+            "flake8-bugbear==24.4.21",
+        ],
+        "types": [
+            "mypy==1.10.0",
         ]
     },
     packages=find_packages(
