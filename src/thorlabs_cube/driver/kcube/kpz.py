20c8e9cdabcf1b575460d0bbb7ee6c966f6eb7f1
armaanrasheed <arasheed@uwaterloo.ca>
2024-09-23 13:41:18 -0400

    changes

---
+++ src/thorlabs_cube/driver/kcube/kpz.py
@@ -0,0 +1,180 @@
+import struct as st
+
+from thorlabs_cube.driver.message import MGMSG, Message
+from thorlabs_cube.driver.tcube.tpz import Tpz, TpzSim
+
+_RESERVED = 0x0
+_CHANNEL = 0x01
+_REQUEST_LENGTH = 1
+
+
+class Kpz(Tpz):
+
+    async def set_kcubemmi_params(
+        self,
+        js_mode,
+        js_volt_gearbox,
+        js_volt_step,
+        dir_sense,
+        preset_volt1,
+        preset_volt2,
+        disp_brightness,
+        disp_timeout,
+        disp_dim_level,
+    ):
+        """Set the KCube MMI parameters.
+
+        :param js_mode: The mode of joystick (JSMODE).
+        :param js_volt_gearbox: The rate of change of voltage for the joystick.
+        :param js_volt_step: The voltage step size when JSMODE is jog mode.
+        :param dir_sense: The direction of joystick motion.
+        :param presetg_volt1: Preset voltage 1 for "Go To" mode.
+        :param preset_volt2: Preset voltage 2 for "Go To" mode.
+        :param disp_brightness: The brightness of the display.
+        :param disp_timeout: The timeout for display dimming.
+        :param disp_dim_level: The dimming level for the display.
+        """
+        payload = st.pack(
+            "<HHHLHLLHHHHHHH",
+            1,
+            js_mode,
+            js_volt_gearbox,
+            js_volt_step,
+            dir_sense,
+            preset_volt1,
+            preset_volt2,
+            disp_brightness,
+            disp_timeout,
+            disp_dim_level,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+        )
+        await self.send(Message(MGMSG.KPZ_SET_KCUBEMMIPARAMS, data=payload))
+
+    async def get_kcubemmi_params(self):
+        """Get the KCube MMI parameters.
+
+        :return: A tuple containing joystick mode, voltage gearbox, voltage
+        step, direction sense, preset voltages, display brightness, timeout,
+        and dim level.
+        """
+        get_msg = await self.send_request(
+            MGMSG.KPZ_REQ_KCUBEMMIPARAMS, [MGMSG.KPZ_GET_KCUBEMMIPARAMS], 1
+        )
+        return st.unpack("<HHHLHLLHHHHHHH", get_msg.data[6:])
+
+    async def set_trigio_config(
+        self, trig1_mode, trig1_polarity, trig2_mode, trig2_polarity
+    ):
+        """Set the TRIG1 and TRIG2 input/output configuration.
+
+        :param trig1_mode: The mode of TRIG1.
+        :param trig1_polarity: The polarity of TRIG1.
+        :param trig2_mode: The mode of TRIG2.
+        :param trig2_polarity: The polarity of TRIG2.
+        """
+        payload = st.pack(
+            "<HHHHHHHHHHH",
+            1,
+            trig1_mode,
+            trig1_polarity,
+            trig2_mode,
+            trig2_polarity,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+        )
+        await self.send(Message(MGMSG.KPZ_SET_KCUBETRIGIOCONFIG, data=payload))
+
+    async def get_trigio_config(self):
+        """Get the TRIG1 and TRIG2 input/output configuration.
+
+        :return: A tuple containing the configuration for TRIG1 and TRIG2 (mode, polarity).
+        """
+        get_msg = await self.send_request(
+            MGMSG.KPZ_REQ_KCUBETRIGIOCONFIG, [MGMSG.KPZ_GET_KCUBETRIGIOCONFIG], 1
+        )
+        return st.unpack("<HHHHHHHHHHH", get_msg.data[6:])
+
+
+class KpzSim(TpzSim):
+
+    def set_kcubemmi_params(
+        self,
+        js_mode,
+        js_volt_gearbox,
+        js_volt_step,
+        dir_sense,
+        preset_volt1,
+        preset_volt2,
+        disp_brightness,
+        disp_timeout,
+        disp_dim_level,
+    ):
+        """Set the KCube MMI parameters in simulation.
+
+        This simulates setting the parameters for the joystick, voltages, and display.
+        """
+        self.js_mode = js_mode
+        self.js_volt_gearbox = js_volt_gearbox
+        self.js_volt_step = js_volt_step
+        self.dir_sense = dir_sense
+        self.preset_volt1 = preset_volt1
+        self.preset_volt2 = preset_volt2
+        self.disp_brightness = disp_brightness
+        self.disp_timeout = disp_timeout
+        self.disp_dim_level = disp_dim_level
+
+    def get_kcubemmi_params(self):
+        """Get the KCube MMI parameters in simulation.
+
+        This returns the simulated parameters for the joystick, voltages, and display.
+        """
+        return (
+            self.js_mode,
+            self.js_volt_gearbox,
+            self.js_volt_step,
+            self.dir_sense,
+            self.preset_volt1,
+            self.preset_volt2,
+            self.disp_brightness,
+            self.disp_timeout,
+            self.disp_dim_level,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+        )
+
+    def set_trigio_config(self, trig1_mode, trig1_polarity, trig2_mode, trig2_polarity):
+        """Set the TRIG1 and TRIG2 configuration in simulation.
+
+        This simulates setting the TRIG1 and TRIG2 modes and polarities.
+        """
+        self.trig1_mode = trig1_mode
+        self.trig1_polarity = trig1_polarity
+        self.trig2_mode = trig2_mode
+        self.trig2_polarity = trig2_polarity
+
+    def get_trigio_config(self):
+        """Get the TRIG1 and TRIG2 configuration in simulation.
+
+        This returns the simulated configuration for the TRIG1 and TRIG2 modes and polarities.
+        """
+        return (
+            self.trig1_mode,
+            self.trig1_polarity,
+            self.trig2_mode,
+            self.trig2_polarity,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+            _RESERVED,
+        )
