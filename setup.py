7a3e361f3110cd12e6749abb14f85eb6e363a6dd
Collin Epstein <cepstein@uwaterloo.ca>
2024-10-10 15:33:25 -0400

    fix asyncserial dependency tag number
    Issue #22
    -fix tag number in git url for asyncserial dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="ARTIQ controller for Thorlabs T/KCube devices",
     install_requires=[
         "sipyco@git+https://github.com/m-labs/sipyco.git@v1.8",
-        "asyncserial@git+https://github.com/m-labs/asyncserial.git@v1.0",
+        "asyncserial@git+https://github.com/m-labs/asyncserial.git@1.0",
     ],
     extras_require={
         "docs": [
