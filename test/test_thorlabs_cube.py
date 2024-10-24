c2608ce13bccb4b473f7e7384e573af035982c84
armaanrasheed <arasheed@uwaterloo.ca>
2024-10-24 14:14:54 -0400

    potential fix for build issue

--- test/test_thorlabs_cube.py
+++ test/test_thorlabs_cube.py
@@ -128,14 +128,17 @@ class GenericTpzTest:
 
 class GenericKpzTest:
     def test_kcubemmi_params(self):
-        test_vector = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0)
+        test_vector = (1, 2, 3, 4, 5, 6, 7, 8, 9)
         self.cont.set_kcubemmi_params(*test_vector)
-        self.assertEqual(test_vector, self.cont.get_kcubemmi_params())
+        expected_result = (*test_vector, 0, 0, 0, 0)  # Adding reserved bytes for comparison
+        self.assertEqual(expected_result, self.cont.get_kcubemmi_params())
+
 
     def test_trigio_config(self):
-        test_vector = (1, 0, 2, 1, 0, 0, 0, 0, 0, 0)
+        test_vector = (1, 0, 2, 1)
         self.cont.set_trigio_config(*test_vector)
-        self.assertEqual(test_vector, self.cont.get_trigio_config())
+        expected_result = (*test_vector, 0, 0, 0, 0, 0, 0)  # Adding reserved bytes for comparison
+        self.assertEqual(expected_result, self.cont.get_trigio_config())
 
 class TestTdcSim(GenericRPCCase, GenericTdcTest):
     def setUp(self):
